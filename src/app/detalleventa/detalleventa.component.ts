cbabe0f0d228e97c8d843101621a78a876ed03ea
jose chunga <josechungacastro@gmail.com>
2024-08-21 17:01:48 -0500

    add client and sale information in salesdetail

--- src/app/detalleventa/detalleventa.component.ts
+++ src/app/detalleventa/detalleventa.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DetalleVentaService } from '../services/detalleventa.service';
 import { DetalleVenta } from '../detalleventa/detalleventa';
+import { Venta } from '../venta/venta';
+import { VentaService } from '../services/venta.service';
 
 @Component({
   selector: 'app-detalleventa',
@@ -10,19 +12,32 @@ import { DetalleVenta } from '../detalleventa/detalleventa';
 })
 export class DetalleVentaComponent implements OnInit {
   detalles: DetalleVenta[] = [];
+  venta: Venta | null = null;
 
   constructor(
     private route: ActivatedRoute,
-    private detalleVentaService: DetalleVentaService
+    private detalleVentaService: DetalleVentaService,
+    private ventaService: VentaService
   ) {}
 
   ngOnInit(): void {
     const ventaId = this.route.snapshot.paramMap.get('id');
     if (ventaId) {
+      this.getVentaById(+ventaId);
       this.getDetallesByVentaId(+ventaId);
     }
   }
 
+  getVentaById(ventaId: number): void {
+    this.ventaService.getVentaById(ventaId).subscribe(
+      (data) => {
+        this.venta = data;
+      },
+      (error) => {
+        console.error('Error al obtener la venta', error);
+      }
+    );
+  }
   getDetallesByVentaId(ventaId: number): void {
     this.detalleVentaService.getAllDetalles().subscribe(
       (data) => {
